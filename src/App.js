42b08fd7d25ca67e51e7d9e25717bc35a7b42782
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-01-29 17:23:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax pages={2} className="animation">
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div className='layer plx' id="h1"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div className='layer plx' id="h2"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div className='layer plx' id="h3"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div className='layer plx' id="h4"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div className='layer plx' id="h5"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.2}>
+          <div className='layer plx' id="leaf"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.6}>
+          <div className='layer plx' id="plant"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div className='layer plx' id="tree"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.2}>
+          <section>
+            <div className='textclass'>
+              Hi there this is parallax website
+            </div>
+          </section>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
